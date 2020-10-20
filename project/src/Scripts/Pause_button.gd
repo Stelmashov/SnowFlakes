f3c5bfc17a48a23d47aad5022c6c4f9adedc4a59
Stelmashov <stelmashov.bogdan@gmail.com>
2020-10-20 11:22:08 +0300

    menu bar

---
+++ project/src/Scripts/Pause_button.gd
@@ -0,0 +1,22 @@
+extends TextureButton
+
+var fl = 0
+
+func _input(event):
+
+	if event is InputEventKey:
+		if (event as InputEventKey).scancode == KEY_ESCAPE:
+			if fl == 0:
+				get_tree().paused = true
+				fl = 1
+			else:
+				get_tree().paused = false
+				fl = 0
+
+func _pressed():
+	if fl == 0:
+		get_tree().paused = true
+		fl = 1
+	else:
+		get_tree().paused = false
+		fl = 0

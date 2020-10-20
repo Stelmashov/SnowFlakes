f69d6b41bc8d0b5f6e5f6eea112e41863d78be39
DefinitelyLD <drobnyyalex@gmail.com>
2020-10-20 22:32:34 +0300

    hud fixes

---
+++ project/src/Utility/Timer/TimerLabel.gd
@@ -0,0 +1,23 @@
+extends Label
+
+
+var seconds = 0
+var minutes = 0
+
+
+func _process(delta):
+	if seconds > 59:
+		minutes += 1
+		seconds = 0
+
+	if seconds <= 9 and minutes <=9:
+		set_text("0"+str(minutes)+":0"+str(seconds))
+	elif seconds <= 9 and minutes > 9:
+		set_text(str(minutes)+":0"+str(seconds))
+	elif seconds > 9 and minutes <= 9:
+		set_text("0"+str(minutes)+":"+str(seconds))
+	else:
+		set_text(str(minutes)+":"+str(seconds))
+
+func _on_seconds_timeout():
+	seconds +=1

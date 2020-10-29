222d8346dbc99d8699c0ffe9aecbf87c7f148e58
Stelmashov <stelmashov.bogdan@gmail.com>
2020-10-29 14:33:21 +0300

    canon rotation limit

--- project/src/cannon/Cannon.gd
+++ project/src/cannon/Cannon.gd
@@ -4,11 +4,11 @@ const DEFAULT_SPEED = 200
 const SPEED_INCREASE_PER_FRAME = 50
 
 var projectile = preload("res://src/cannon/projectile/snowflake.tscn")
-onready var shoot_part = get_node("Sprite/shoot")
 var is_ready = true
 var hold_time = 0.0
 var speed = DEFAULT_SPEED
 
+onready var shoot_part = get_node("Sprite/shoot")
 onready var level = get_tree().get_root().get_node("World")#mb can switch to owner
 onready var crosshair = level.get_node("Crosshair")
 onready var INTERVAL = $INTERVAL
@@ -18,8 +18,6 @@ func shoot():
 	level.call_deferred("add_child", projectile_instance)
 	projectile_instance.transform = global_transform
 	projectile_instance.direction = (crosshair.global_position - global_position).normalized()
-
-
 
 func _physics_process(delta):
 	if Input.is_action_pressed("attack"):
@@ -32,7 +30,6 @@ func _physics_process(delta):
 		is_ready = false
 		shoot_part.emitting = true
 		INTERVAL.start(2)
-
 
 func _on_INTERVAL_timeout():
 	is_ready = true
--- project/src/cannon/rotation.gd
+++ project/src/cannon/rotation.gd
@@ -1,4 +1,11 @@
 extends Sprite
 
+var new_rotation
+
+onready var cannon = get_node("../")
+
 func _process(delta):
-	rotation = owner.get_angle_to(owner.crosshair.global_position)
+	if cannon.is_ready:
+		new_rotation = owner.get_angle_to(owner.crosshair.global_position)
+		if new_rotation >= deg2rad(-90) and new_rotation <= deg2rad(0):
+			rotation = new_rotation
